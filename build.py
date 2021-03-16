07d198525e43e2712ed497ac343abbd5d8ec889c
Chris Marsh <chrismarsh.c2@gmail.com>
2021-03-16 15:23:00 -0600

    Per OS detection and packaging

--- build.py
+++ build.py
@@ -1,26 +1,15 @@
 from cpt.packager import ConanMultiPackager
 from collections import defaultdict
-import platform
+
 
 if __name__ == "__main__":
-
-    command=""
-
     builder = ConanMultiPackager(cppstds=[14],
                                 archs=["x86_64"],
-                                build_types=["Release"],
-                                docker_entry_script=command)
-                              
+                                build_types=["Release"])
+                        
+    builder.add_common_builds(pure_c=False, shared_option_name=False)
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") # old ABI
 
-    builder.add_common_builds(pure_c=False,shared_option_name=False)
-
-    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
-
-    named_builds = defaultdict(list)
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        named_builds[settings['compiler']].append([settings, options, env_vars, build_requires, reference])
-
-    builder.named_builds = named_builds
 
     builder.run()
 
